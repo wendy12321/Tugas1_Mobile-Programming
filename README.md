ead7c44eea5ace804f7c09eedafc6b8fcb7d8bc2
wendy12321 <88644797+wendy12321@users.noreply.github.com>
2022-02-21 23:01:12 +0700

    Tugas 1 - Instalasi dan Program Sederhana
    Tugas 1 - Instalasi dan Program Sederhana
    
    Install dan pastikan Flutter dapat berjalan pada komputer anda masing-masing. Ikutilah materi yang diberikan pada ketiga video tersebut. Ketikkan kode programnya dan masukkan ke dalam Github masing-masing.
    
    Kumpulkan link ke repository Github anda yang berisi kode program dari contoh yang ada pada video tersebut.

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# Tugas1_Mobile-Programming-
-Tugas 1 - Instalasi dan Program Sederhana 
+# 535200066 - Wendy Bunandi
+
+# Tugas1_Mobile-Programming
+
+Tugas 1 - Instalasi dan Program Sederhana
+
+Install dan pastikan Flutter dapat berjalan pada komputer anda masing-masing. Ikutilah materi yang diberikan pada ketiga video tersebut. Ketikkan kode programnya dan masukkan ke dalam Github masing-masing.
+
+Kumpulkan link ke repository Github anda yang berisi kode program dari contoh yang ada pada video tersebut.
---
+++ flutter_application_1/lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter Demo'),
+          backgroundColor: Colors.teal,
+        ),
+        // backgroundColor: Colors.black,
+        body: Center(
+          child: Column(
+            children: [
+              Image(
+                image: NetworkImage(
+                    'https://images.pexels.com/photos/348601/pexels-photo-348601.jpeg?cs=srgb&dl=pexels-sindhu-wijaya-348601.jpg&fm=jpg'),
+              ),
+
+              Text('Hello, World!'),
+
+              Image(
+                image: AssetImage('images/logofti.png'),
+              ),
+
+              Text('Hello, World!'),
+
+              // Image(
+              //   image: AssetImage('images/logopanda.jpg'),
+              // ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
